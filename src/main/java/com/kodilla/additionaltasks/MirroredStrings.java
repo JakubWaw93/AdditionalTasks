08c386cdb672d16ed265b9b3b7a384e2cfca16a5
Jakub Wawryniuk <jakubwaw93@gmail.com>
2023-11-19 15:44:15 +0100

    7.6 MirroredStrings

---
+++ src/main/java/com/kodilla/additionaltasks/MirroredStrings.java
@@ -0,0 +1,18 @@
+package com.kodilla.additionaltasks;
+
+public class MirroredStrings {
+
+    public static void mirrored(String s){
+        StringBuilder sb = new StringBuilder();
+        for(int i=s.length()-1; i>=0; i--){
+            sb.append(s.charAt(i));
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        String example = "Jerozolima";
+
+        mirrored(example);
+    }
+}
